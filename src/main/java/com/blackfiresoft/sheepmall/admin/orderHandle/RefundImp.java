8aca2750c3ee4fac4c278c5b67efcbe3dbbb57b0
Ben <Beng9778@outlook.com>
2025-02-17 16:00:18 +0800

    update: refund status

--- src/main/java/com/blackfiresoft/sheepmall/admin/orderHandle/RefundImp.java
+++ src/main/java/com/blackfiresoft/sheepmall/admin/orderHandle/RefundImp.java
@@ -1,8 +1,12 @@
 package com.blackfiresoft.sheepmall.admin.orderHandle;
 import com.blackfiresoft.sheepmall.factory.RefundFactory;
+import com.blackfiresoft.sheepmall.payment.model.RefundOrderRqs;
+import com.blackfiresoft.sheepmall.payment.service.PayService;
 import com.blackfiresoft.sheepmall.refund.Refund;
 import com.blackfiresoft.sheepmall.refund.RefundRepository;
 import com.blackfiresoft.sheepmall.user.Users;
+import com.blackfiresoft.sheepmall.util.StatusEnum;
+import com.wechat.pay.java.service.refund.model.Status;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -41,6 +45,24 @@ public class RefundImp implements RefundFactory {
     @Transactional(rollbackFor = Exception.class)
     public void updateRefundStatus(Long refundId,String status) {
         refundRepository.findById(refundId).ifPresent(refund -> {
+            if(status.equals(StatusEnum.AGREE.toString())){
+                //同意退款操作
+                PayService payService = new PayService();
+                RefundOrderRqs rqs = new RefundOrderRqs();
+                rqs.setOutRefundNo(refund.getRefundNo());
+                rqs.setOutTradNo(refund.getOrderNo());
+                rqs.setTotal((long)refund.getRefundAmount().intValueExact());
+                rqs.setRefund((long)refund.getRefundAmount().intValueExact());
+                com.wechat.pay.java.service.refund.model.Refund refundResult = payService.create(rqs);
+                if(refundResult.getStatus().equals(Status.SUCCESS)){
+                    //退款成功
+                    refund.setStatus(StatusEnum.REFUND_SUCCESS.toString());
+                }
+                refund.setStatus(StatusEnum.REFUNDING.toString());
+            } else if (status.equals(StatusEnum.REJECTED.toString())) {
+                //拒绝退款操作
+                refund.setStatus(StatusEnum.REJECTED.toString());
+            }
             refund.setStatus(status);
             refundRepository.saveAndFlush(refund);
         });
