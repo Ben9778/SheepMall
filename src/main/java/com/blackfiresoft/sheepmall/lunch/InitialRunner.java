c04decf397f6dd12a351516828936a86cb6025c0
Ben <Beng9778@outlook.com>
2025-02-17 15:53:16 +0800

    feat: load pay params

--- src/main/java/com/blackfiresoft/sheepmall/lunch/InitialRunner.java
+++ src/main/java/com/blackfiresoft/sheepmall/lunch/InitialRunner.java
@@ -1,15 +1,23 @@
 package com.blackfiresoft.sheepmall.lunch;
 
+import com.blackfiresoft.sheepmall.payment.InitPayProperty;
+import com.blackfiresoft.sheepmall.payment.PayRqsRepository;
+import jakarta.annotation.Resource;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
 /**
  * 初始化加载支付配置参数
  */
+@Component
 public class InitialRunner implements ApplicationRunner {
+    @Resource
+    private PayRqsRepository payRqsRepository;
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-
+    public void run(ApplicationArguments args) {
+        // 初始化支付配置参数
+        payRqsRepository.findById(1L).ifPresent(InitPayProperty::setPayProperty);
     }
 }
