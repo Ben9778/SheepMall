4e1e5325417f92dc8124edbfc9d4896e35c07ae5
Ben <Beng9778@outlook.com>
2025-02-17 16:00:48 +0800

    update

--- src/test/java/com/blackfiresoft/sheepmall/SheepMallApplicationTests.java
+++ src/test/java/com/blackfiresoft/sheepmall/SheepMallApplicationTests.java
@@ -1,6 +1,4 @@
 package com.blackfiresoft.sheepmall;
-import com.blackfiresoft.sheepmall.receipt.ReceiptRepository;
-import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
